2cd004f312aff07802dbe19f677c12a9c5f9c95a
Heri Susanto <susantoh41@gmail.com>
2020-02-20 19:49:46 +0700

    Add rooms page

---
+++ src/views/StartGame.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="card text-center">
+      <div class="card-header ">
+        <h1 class="my-0 font-weight-normal">Rooms</h1>
+      </div>
+      <div class="card-body">
+          
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
